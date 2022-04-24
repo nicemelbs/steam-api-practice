05898ac850909cc9ed27982fac2bc67115cc2dac
Melvin San Jose <sanjose.melvin@gmail.com>
2022-04-24 20:23:12 +0800

    Added notice when viewing friends list for private profiles.

--- views/userFriends.php
+++ views/userFriends.php
@@ -13,27 +13,36 @@ $steamUser->fetchUserFriends();
 ?>
 
 <div class="container">
-    <h3><?= $steamUser->personaname ?>'s Friends</h3>
+    <?php if (count($steamUser->friends) > 0): ?>
 
-    <table class="table-striped">
-        <tr>
-            <th>Photo</th>
-            <th>Name</th>
-            <th>Friends Since</th>
-            <th>Steam Profile</th>
-        </tr>
+        <h3><?= $steamUser->personaname ?>'s Friends</h3>
 
-        <?php
-        foreach ($steamUser->friends as $steamFriend) {
-            $friend = SteamUser::findBySteamId($steamFriend->steamid);
+        <table class="table-striped">
+            <tr>
+                <th>Photo</th>
+                <th>Name</th>
+                <th>Friends Since</th>
+                <th>Steam Profile</th>
+            </tr>
 
-            echo '<tr>' . PHP_EOL;
-            echo '<td><img src="' . $friend->avatar . '" alt="' . $friend->personaname . '" /></td>';
-            echo '<td><a href="/profile/' . $steamFriend->steamid . '">' . $friend->personaname . '</a></td>';
-            echo '<td>' . date('F j, Y', $steamFriend->friend_since) . '</td>';
-            echo '<td><a href="' . $friend->profileurl . '">' . $friend->profileurl . '</a></td>';
-            echo '</tr>' . PHP_EOL;
-        }
-        ?>
-    </table>
+            <?php
+            foreach ($steamUser->friends as $steamFriend) {
+                $friend = SteamUser::findBySteamId($steamFriend->steamid);
+
+                echo '<tr>' . PHP_EOL;
+                echo '<td><img src="' . $friend->avatar . '" alt="' . $friend->personaname . '" /></td>';
+                echo '<td><a href="/profile/' . $steamFriend->steamid . '">' . $friend->personaname . '</a></td>';
+                echo '<td>' . date('F j, Y', $steamFriend->friend_since) . '</td>';
+                echo '<td><a href="' . $friend->profileurl . '">' . $friend->profileurl . '</a></td>';
+                echo '</tr>' . PHP_EOL;
+            }
+            ?>
+        </table>
+    <?php else: ?>
+        <div class="container">
+            <div class="alert-info">
+                <?= $steamUser->personaname ?> either has no friends or their profile is set to private.
+            </div>
+        </div>
+    <?php endif; ?>
 </div>
